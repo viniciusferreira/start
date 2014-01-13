62cc90c6a67ff64092e8b218403bfb0936267f2d
Bill Rocha <ahcordesign@gmail.com>
2014-01-13 16:07:31 -0200

    version 0.0.1

---
+++ php/lib/neos/db/conn.php
@@ -0,0 +1,17 @@
+<?php
+namespace Lib\Neos\Db;
+
+/* CONN
+ * TODO: In construction!
+ * Do you have any idea? 
+ * prbr@ymail.com
+ */
+
+class Conn {
+
+    function __cosntruct(){
+      //TODO
+    }
+
+
+}
